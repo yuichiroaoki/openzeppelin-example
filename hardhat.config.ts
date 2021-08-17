09e53a6ae61f2c2ed22dcc98c6e95458bcde8f02
Yuichiro Aoki <45054071+yuichiroaoki@users.noreply.github.com>
2021-08-17 10:55:48 +0900

    Initial commit

---
+++ hardhat.config.ts
@@ -0,0 +1,24 @@
+import { config as dotEnvConfig } from "dotenv";
+dotEnvConfig();
+import "@nomiclabs/hardhat-etherscan";
+import "@nomiclabs/hardhat-waffle";
+import "@nomiclabs/hardhat-ethers";
+import '@typechain/hardhat'
+import "solidity-coverage"
+
+import "./tasks/accounts";
+import "./tasks/balance";
+import "./tasks/block-number";
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.8.4",
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY
+  },
+};
